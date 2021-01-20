d028a2fd5635c52f72c0031708dc86734235a1dc
faiza203 <fiazfaiza981@gmail.com>
2021-01-20 11:10:09 -0500

    Run app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
 
 const port = process.env.PORT | 8000;
 const app = express();
+
+app.use("/graphql", graphqlHTTP({}))
 
 app.listen(port , () => {
     console.log(`I am running on port ${port}`);
