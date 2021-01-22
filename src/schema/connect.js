c3d17b28e8743b79111e3bc074d6df8d48de8bef
faiza203 <fiazfaiza981@gmail.com>
2021-01-22 10:29:08 -0500

    Create connect.js and  app connects with moongoose

---
+++ src/schema/connect.js
@@ -0,0 +1,11 @@
+const mongoose = require("moongoose");
+
+mongoose.connect("mongodb://localhost:27017/books" , {
+  useNewUrlParser : true,
+  useUnifiedTopology : true,
+  useCreateIndex : true,  
+}).then(() => {
+    console.log("Connected with mongodb");
+}).catch((e) =>{
+    console.log("Not Connection");
+})
